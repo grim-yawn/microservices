010fb6f2e0b8a58e0d92be9c6707e5104f043bc9
Ivan Kosolapov <kosolapow.iwan@gmail.com>
2018-08-17 12:03:01 +0300

    Optimize images size

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
 IFS=$'\n\t'
 
 # Enable docker host
@@ -18,16 +18,21 @@ docker build -t "${BASE_NAME}/ui:1.0" ./ui
 
 # Create network if not exists
 NETWORK=reddit
-
 if ! docker network ls | grep "${NETWORK}" > /dev/null; then
-    docker network create ${NETWORK}
+    docker network create "${NETWORK}"
+fi
+
+# Create volume for mongo
+VOLUME=reddit_db
+if ! docker volume ls | grep "${VOLUME}" > /dev/null; then
+    docker volume create "${VOLUME}"
 fi
 
 # Stop containers
 docker kill $(docker ps -q) || true
 
 # Run containers
-docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db -v "${VOLUME}:/data/db" mongo:latest
 docker run -d --network=reddit --network-alias=post "${BASE_NAME}/post:1.0"
 docker run -d --network=reddit --network-alias=comment "${BASE_NAME}/comment:1.0"
 docker run -d --network=reddit -p 9292:9292 "${BASE_NAME}/ui:1.0"
